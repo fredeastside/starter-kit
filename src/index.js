e68fe6cd817096f72ddde8c3dbf971bca566ea10
s.kaznakhovskiy <sergey.kaznakhovskiy@kupivip.ru>
2016-03-22 22:06:57 +0300

    dev server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+/*
+reqiure.ensure([], function(require) {
+  // динамическая подгрузка
+  require('./app');
+}, 'auth' - название для общей сборки кусков);
+*/
+import React from 'react';
+
 console.log(123);
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,29 @@ const webpack = require('webpack');
 module.exports = {
   context: __dirname + "/src",
   entry: "./index",
+  /*
+  несколько точек входа
+  enrty: {
+    home: "home",
+    about: "about",
+  }
+  */
   output: {
       path: __dirname + "/dist",
-      filename: "bundle.js"
+      publicPath: '/',
+      //filename: "bundle.[chunkhash].js" // filename: "[name].js" несколько точек входа
+      filename: "bundle.js" // filename: "[name].js" несколько точек входа
+      // chunkFilename: "[id].[chunkhash].js"
+      // library: "[name]" // глобальные переменные
   },
   /*
   watch: NODE_ENV === 'development',
   watchOptions: {
     aggregateTimeout: 100
+  },
+  // cdn
+  externals: {
+    lodash: "_"
   },
   devtool: NODE_ENV === 'development' ? "cheap-inline-module-source-map" : null
   //"cheap-inline-module-source-map" or "eval" in dev, "source-map" in prod
@@ -21,9 +36,15 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel',
-        exclude: /node_modules/,
-      }
+        //exclude: /node_modules/,
+        include: __dirname + "/src"
+      }/*,
+      {
+        test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+        loader: 'file?name=[path][name].[hash:6].[ext]'
+      }*/
     ]
+    // noParse:
   },
   resolve: {
     modulesDirectories: ['node_modules'],
@@ -35,10 +56,19 @@ module.exports = {
     extensions: ['', '.js', '.jsx']
   },
   plugins: [
+    new webpack.NoErrorsPlugin(), // не пересобирать js если возникли ошибки
     new webpack.DefinePlugin({
       NODE_ENV: JSON.stringify(NODE_ENV)
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "common"
     })
-  ]
+  ],
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    contentBase: __dirname + '/dist'
+  }
 };
 
 if (NODE_ENV === 'production') {
@@ -47,7 +77,7 @@ if (NODE_ENV === 'production') {
       compress: {
         warnings: false,
         drop_console: true,
-        unsafe: true  
+        unsafe: true
       }
     })
   );
